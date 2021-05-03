47f77d1b41ae8e5e8aa592742e5b6752132f3f56
qwbarch <edwardyang0410@gmail.com>
2021-05-03 17:36:10 -0400

    Add missing loop

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import logging
 from datetime import date, timedelta
 
+from win10toast import ToastNotifier
+
 from location import Location
 from priority_area import PriorityArea
 from vaccine_api import VaccineApi
-from win10toast import ToastNotifier
 
 ########### Specify your search details here ###########
-location = Location.CENTENNIAL_COLLEGE
+location = Location.CAREFIRST_SENIORS
 priority_area = PriorityArea.ELIGIBLE_AGE_GROUPS
 ########################################################
 
@@ -17,12 +18,15 @@ toast = ToastNotifier()
 
 # Run the script until an available slot is found
 with VaccineApi() as vaccine_api:
-    today = date.today()
-    # The website checks over the current date + the next 2 weeks, so this is what we'll do as well
-    for i in range(1, 15):
-        check_date = today + timedelta(days=i)
-        available_slots = vaccine_api.find_available_slots(location, priority_area, check_date)
-        if (available_slots != 0):
-            found_message = f"{available_slots} slot(s) are available on {check_date}"
-            logger.info(found_message)
-            toast.show_toast("Vaccination Appointment Found!", found_message)
+    is_slots_found = False
+    while not is_slots_found:
+        today = date.today()
+        # The website checks over the current date + the next 2 weeks, so this is what we'll do as well
+        for i in range(1, 15):
+            check_date = today + timedelta(days=i)
+            available_slots = vaccine_api.find_available_slots(location, priority_area, check_date)
+            if (available_slots != 0):
+                is_slots_found = True
+                found_message = f"{available_slots} slot(s) are available on {check_date}"
+                logger.info(found_message)
+                toast.show_toast("Vaccination Appointment Found!", found_message)
