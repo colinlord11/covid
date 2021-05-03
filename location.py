4b0aa05bc3235ac00d3bcd8d9b60d3f517a0f3a8
Edward Yang <edwardyang0410@gmail.com>
2021-05-02 23:48:02 -0400

    Add location enum

---
+++ location.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Location(Enum):
+    SHN_CENTENARY_HOSPITAL = "CHV"
+    CENTENNIAL_COLLEGE = "CCV"
+    CAREFIRST_SENIORS = "CSV"
+    SCARBOROUGH_CENTRE = "BEV"
+    TAIBU_COMMUNITY_HEALTH_CENTRE = "TCV"
